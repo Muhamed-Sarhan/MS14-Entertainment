1452e00915bed49ed327e648f2831c1ae23f780e
Muhamed-Sarhan <muhamedsarhan14@gmail.com>
2021-12-25 00:42:23 +0200

    New release in app

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./footer.scss";
 
 import { Link } from "react-router-dom";
 import bg from "../../assets/footer-bg.jpg";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/mylogo.png";
 
 const Footer = () => {
   return (
@@ -12,8 +12,9 @@ const Footer = () => {
       <div className="footer__content container">
         <div className="footer__content__logo">
           <div className="logo">
-            <img src={logo} alt="logo" />
-            <Link to="/">MS14</Link>
+            <Link to="/">
+              <img src={logo} alt="logo" />
+            </Link>
           </div>
         </div>
         <div className="footer__content__menus">
